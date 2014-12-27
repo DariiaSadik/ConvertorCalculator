f019ba614a5594ce210feb9e2cf629ce85fd5308
DariiaSadik <dasha.sadik@gmail.com>
2014-12-27 20:44:23 +0200

    ConvertorCalculator test

--- src/main/java/com/sourceit/calculation/PolishStrategy.java
+++ src/main/java/com/sourceit/calculation/PolishStrategy.java
@@ -90,7 +90,7 @@ public class PolishStrategy {
                 while (!op.isEmpty() && priority(op.getLast()) >= priority(c)) {
                     processOperator(st, op.removeLast());
                 }
-                op.add(c);  // test
+                op.add(c);  // PUSH
             } else {
                 String value = "";
                 String type = "";
